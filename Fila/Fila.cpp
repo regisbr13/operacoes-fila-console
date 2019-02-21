9331b6ac4a5063d952be8cf51f9e8a24e6f07dc6
Regis Lima <regislima1391@gmail.com>
2019-02-21 10:24:02 -0300

    Definido menu de interacao com o usuario

--- Fila/Fila.cpp
+++ Fila/Fila.cpp
@@ -5,6 +5,46 @@ using namespace std;
 
 int main()
 {
-    
+	int opcao = 1;
+
+	while (opcao != 0) {
+
+		cout << "####### Operacoes com Fila #######\n \n";
+		cout << "|||||||||||||||||||||||||||||||||||||\n\n";
+		cout << "MENU\n\n";
+
+		cout << "0 - Sair \n";
+		cout << "1 - Enfileirar \n";
+		cout << "2 - Desenfileirar \n";
+		cout << "3 - Mostrar fila \n";
+		cout << "4 - Exibir inicio \n";
+		cout << "5 - Exibir a quantidade \n";
+		cout << "6 - Esvaziar \n";
+		cout << "\n";
+		cout << "Opcao: ";
+		cin >> opcao;
+
+		if (opcao == 0) {
+			cout << "Fim do programa";
+		}
+		else if (opcao == 1) {
+		}
+		else if (opcao == 2) {
+		}
+		else if (opcao == 3) {
+		}
+		else if (opcao == 4) {
+		}
+		else if (opcao == 5) {
+		}
+		else if (opcao == 6) {
+		}
+		else {
+			cout << "Opçao Invalida";
+		}
+		system("PAUSE");
+		system("cls");
+	}
+	return 0;
 }
 
